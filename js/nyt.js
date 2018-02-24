d40620ef75282b30ac96c91d73df1956da053150
anchi84 <anarili@gmail.com>
2018-02-24 13:56:06 -0800

    find fixed

--- js/nyt.js
+++ js/nyt.js
@@ -3,20 +3,16 @@ class Response extends React.Component {
             constructor(props) {
                 super(props);
                 this.state = {articles: []};
+                this.refresh = this.refresh.bind(this);
             }
 
-            componentDidMount() {
-                var date = document.getElementById('date').value;
-                console.log(date);
-                var year = date.split("-")[0];
-                var month = date.split("-")[1];
-                if(month[0] == "0") {
-                    month = month[1];
-                }
-                console.log(year);
-                console.log(month);
+            componentWillReceiveProps(nextProps){
+                this.refresh(nextProps.year, nextProps.month);                
+            }
+
+            refresh(y, m){
                 $.ajax({
-                    url: "https://api.nytimes.com/svc/archive/v1/"+year +"/" +month +".json?api-key=be3ab30bcbd64f3492955d2aa48b0567",
+                    url: "https://api.nytimes.com/svc/archive/v1/"+y +"/" +m +".json?api-key=be3ab30bcbd64f3492955d2aa48b0567",
                     method: 'GET',
                 }).done((result)=> {
                             this.setState({articles: result.response.docs});
@@ -24,9 +20,15 @@ class Response extends React.Component {
                          }).fail(function(err) {
                             throw err;
                     });
+
+            }
+
+            componentDidMount() {
+                this.refresh(this.props.year, this.props.month);
             }
 
             render() {
+                console.log('render');
                 const articles = this.state.articles.slice(0, 20);
                 // return (articles.map((article,index) => <a href={article.web_url} target="_blank" key={index}>{article.headline.main}<br/></a>));
                 return <Articles articles={articles.map(article => article.web_url)}/>;   
@@ -50,6 +52,17 @@ function Url(props){
 }
 
 $('#find').click(function() {
-        const root = document.getElementById('root');
-        ReactDOM.render(<Response/>,  root);
+
+    var date = document.getElementById('date').value;
+    console.log(date);
+    var year = date.split("-")[0];
+    var month = date.split("-")[1];
+    if(month[0] == "0") {
+        month = month[1];
+    }
+    console.log(year);
+    console.log(month);
+
+    const root = document.getElementById('root');
+    ReactDOM.render(<Response year={year} month={month} />,  root);    
 });
