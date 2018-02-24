8e9c539aff12ebc2beabb45234ccfec920bb4776
anchi84 <anarili@gmail.com>
2018-02-24 12:09:48 -0800

    functional components added

--- js/nyt.js
+++ js/nyt.js
@@ -29,7 +29,7 @@ class Response extends React.Component {
             render() {
                 const articles = this.state.articles.slice(0, 20);
                 // return (articles.map((article,index) => <a href={article.web_url} target="_blank" key={index}>{article.headline.main}<br/></a>));
-                return <Articles articles={articles.map(article => article.web_url)};   
+                return <Articles articles={articles.map(article => article.web_url)}/>;   
             }
 }
 
