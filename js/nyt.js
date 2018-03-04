7d63fdc4e87c13aae02010f1085e25af33293bfa
anchi84 <anarili@gmail.com>
2018-03-03 16:37:22 -0800

    selected article modified

--- js/nyt.js
+++ js/nyt.js
@@ -4,7 +4,18 @@ class Response extends React.Component {
             super(props);
             this.state = {
                 articles: [],
-                selectedArticle: "Click on image to see article details!"     
+                selectedArticle: {
+                    new_desk: "",
+                    snippet: "",
+                    pub_date: "",
+                    web_url: "",
+                    byline: {
+                        original: ""
+                    },
+                    headline: {
+                        main: ""
+                    }
+                }     
             };
             this.refresh = this.refresh.bind(this);
             this.handleImgClick = this.handleImgClick.bind(this);
@@ -13,8 +24,8 @@ class Response extends React.Component {
     componentWillReceiveProps(nextProps) {
         if(this.props.year != nextProps.year || this.props.month != nextProps.month) {
             this.refresh(nextProps.year, nextProps.month);
-            }                
-        }
+        }                
+    }
 
     refresh(y, m){
         $.ajax({
@@ -34,8 +45,8 @@ class Response extends React.Component {
     }
 
     handleImgClick(item) {
-        console.log("hello from handleImgClick")
-        this.setState({selectedArticle: item})
+        console.log("hello from handleImgClick");
+        this.setState({selectedArticle: item});
     }
 
     render() {
@@ -46,7 +57,7 @@ class Response extends React.Component {
         return (
             <div className="response">
                 <div className="master">
-                    <Articles articles={articles} onImgClick={this.handleImgClick}/>
+                    <Articles articles={articles} onImgClick={this.handleImgClick} selectedID={this.state.selectedArticle._id}/>
                 </div>
                 <div className="split"/>
                 <div className="details">
@@ -60,6 +71,7 @@ class Response extends React.Component {
 class Articles extends React.Component {
     render() {
         const onImgClick = this.props.onImgClick;
+        const selectedID = this.props.selectedID;
         return (
             <div> 
                 {
@@ -67,7 +79,8 @@ class Articles extends React.Component {
                         (article, index) => <Url key={index}
                                             article={article} 
                                             url={article.web_url} 
-                                            clickHandler={onImgClick}/>
+                                            clickHandler={onImgClick}
+                                            selectedID={selectedID}/>
                     )
                 }
             </div>
@@ -84,25 +97,25 @@ class Url extends React.Component {
 
     componentDidMount() {
         $.ajax({
-        url: "https://api.linkpreview.net?key=5a8c62f97676dad065b7f42514cf709a26b3bb95f39ee&q="+this.props.url,
-        // url: "https://api.linkpreview.net?key=123456&q=https://www.google.com",
+        // url: "https://api.linkpreview.net?key=5a8c62f97676dad065b7f42514cf709a26b3bb95f39ee&q="+this.props.url,
+        url: "https://api.linkpreview.net?key=123456&q=https://www.google.com",
         method: 'GET'
         }).done((result)=> {
-                    this.setState({data: result});
-                    console.log("Hello from LinkPreview");
-                    console.log(result);
-                    }).fail(function(err) {
-                    throw err;
-            });
+            this.setState({data: result});
+            console.log("Hello from LinkPreview");
+            console.log(result);
+        }).fail(function(err) {
+            throw err;
+        });
     }
 
     render() {
         // return <p><a href={this.props.url} target="_blank">{this.props.url}</a></p>;
         const clickHandler = this.props.clickHandler;
         return (  
-            <div className="preview">
+            <div className={"preview" + (this.props.selectedID === this.props.article._id ? " selected-article":"")}>
                 <h1>{this.state.data.title}</h1>
-                <img src={this.state.data.image} onClick={()=>clickHandler(this.props.article)} alt="NY Times Article" width="50%"/>
+                <img className="article-image" src={this.state.data.image} onClick={()=>clickHandler(this.props.article)} alt="NY Times Article" width="50%"/>
                 <p>{this.state.data.description}</p>
             </div>
         )
@@ -110,14 +123,19 @@ class Url extends React.Component {
 }
 
 const ArticleDetails = (props) => {
-            const article = props.article;
-            console.log("hello from ArticleDetails")
-            return (
-                <div>
-                    <p>Article details:</p>
-                    <p>{JSON.stringify(article, null, '\t')}</p>
-                </div>
-            );
+    const article = props.article;
+    console.log("hello from ArticleDetails");
+    return (
+        <div>
+            <p>Article details:</p>
+            {/*<p>{JSON.stringify(article, null, '\t')}</p>*/}
+            <h1>{article.new_desk}</h1>
+            <p>{article.snippet}</p>
+            <p>{article.byline.original}</p>
+            <p>{article.pub_date.split("T")[0].replace(/-/g, "/")}</p>
+            <a href={article.web_url} target="_blank">{article.headline.main}</a>
+        </div>
+    );
 }
 
 $('#find').click(function() {
